3dcef375a2767e1604886e7b82abc2fb25521eb6
youssefoumate <ysf.ing.ysf@gmail.com>
2023-02-23 15:13:22 +0900

    compare two trees

--- b_trees/b_tree_traversal.py
+++ b_trees/b_tree_traversal.py
@@ -3,8 +3,6 @@ class Tree():
     def __init__(self):
         self.traversed_nodes = [] 
         self.list_proc = [] 
-        self.root = None
-        self.init_tree()
 
     class Node:
         def __init__(self, data=None, left=None, right=None):
@@ -12,15 +10,16 @@ class Tree():
             self.left = left	# pointer to the left child
             self.right = right	# pointer to the right child
 
-    def init_tree(self):
-        self.root = self.Node(1)
-        self.root.left = self.Node(2)
-        self.root.left.left = self.Node(4)
-        self.root.right = self.Node(3)
-        self.root.right.left = self.Node(5)
-        self.root.right.left.left = self.Node(7)
-        self.root.right.left.right = self.Node(8)
-        self.root.right.right = self.Node(6)
+    def init_tree(self, v_root):
+        root = self.Node(v_root)
+        root.left = self.Node(2)
+        root.left.left = self.Node(4)
+        root.right = self.Node(3)
+        root.right.left = self.Node(5)
+        root.right.left.left = self.Node(7)
+        root.right.left.right = self.Node(8)
+        root.right.right = self.Node(6)
+        return root
 
     def inorder_tree_traversal(self, root):
         if root is None:
@@ -43,6 +42,17 @@ class Tree():
         self.postorder_tree_traversal(root.right)
         self.list_proc.append(root.data)
 
+    def cmp_trees(self, root1, root2):         
+        if root1 is None or root2 is None:
+            return
+        self.cmp_trees(root1.left, root2.left)
+        if root1.data != root2.data:
+            return False
+        self.cmp_trees(root1.right, root2.right)
+        return True
+
 tree = Tree()
-tree.postorder_tree_traversal(tree.root)
-print(tree.list_proc)
+root1 = tree.init_tree(5)
+root2 = tree.init_tree(7)
+print(tree.cmp_trees(root1, root2))
+#print(tree.list_proc)
